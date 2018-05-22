7ab9bb15c3a7dccd6f157f3980100c53d6e6fb33
Natalia Castro <nat.alvz@gmail.com>
2018-05-22 21:33:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.each do |season, holiday_hash|
+  holiday_hash
 end
 
 
