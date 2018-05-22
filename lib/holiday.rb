d1132cec04cf219b6994140e5c429860db6fdc01
Natalia Castro <nat.alvz@gmail.com>
2018-05-22 21:21:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   winter_supplies = []
   holiday_hash.each { |season, holiday| 
-  puts "#{"
+  puts "#{season"
 end
 
 def all_holidays_with_bbq(holiday_hash)
