1436f67b73a204c58ad9442ca206affb23498a51
Natalia Castro <nat.alvz@gmail.com>
2018-05-22 21:34:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.each do |season, holiday_hash|
   holiday_hash.each do |holiday, supply|
-    bbq << holiday if supply.to_s.include?
+    bbq << holiday if supply.to_s.include?("BBQ")
 end
 
 
